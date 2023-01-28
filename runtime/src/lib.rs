75244b7a62ef02a988b0774f4c8d0a185b2a6f68
kia9372 <kiadr9372@gmail.com>
2023-01-28 18:29:05 +0200

    remove runtime-api package

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -676,43 +676,6 @@ impl_runtime_apis! {
 		}
 	}
 
-
-	impl pallet_contracts_rpc_runtime_api::ContractsApi<Block, AccountId, Balance, BlockNumber>
-	for Runtime
-{
-	fn call(
-		origin: AccountId,
-		dest: AccountId,
-		value: Balance,
-		gas_limit: u64,
-		input_data: Vec<u8>,
-	) -> ContractExecResult {
-		let (exec_result, gas_consumed) =
-			Contracts::bare_call(origin, dest.into(), value, gas_limit, input_data);
-		match exec_result {
-			Ok(v) => ContractExecResult::Success {
-				flags: v.flags.bits(),
-				data: v.data,
-				gas_consumed: gas_consumed,
-			},
-			Err(_) => ContractExecResult::Error,
-		}
-	}
-
-	fn get_storage(
-		address: AccountId,
-		key: [u8; 32],
-	) -> pallet_contracts_primitives::GetStorageResult {
-		Contracts::get_storage(address, key)
-	}
-
-	fn rent_projection(
-		address: AccountId,
-	) -> pallet_contracts_primitives::RentProjectionResult<BlockNumber> {
-		Contracts::rent_projection(address)
-	}
-}
-
 }
 
 #[cfg(test)]
